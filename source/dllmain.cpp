84c6a0d1d0e025b7940617322ac31e5fd14e5307
John Duffy <johnduffyx@outlook.com>
2020-02-21 09:04:43 -0500

    Fix windows build.

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -1,6 +1,3 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#include "stdafx.h"
-
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
 
